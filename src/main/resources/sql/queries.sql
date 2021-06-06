ee0cf9f4e4999c0810756f24b7ea5fb7f18e0577
ypiel <ypiel@talend.com>
2021-06-06 18:45:30 +0200

    Downgrade derby version to be ireport compliant/java8.

--- src/main/resources/sql/queries.sql
+++ src/main/resources/sql/queries.sql
@@ -3,3 +3,10 @@ select YEAR(date) as y, MONTH(date) as m, sum(LINKEDENTRY_INTEREST) as interests
     from invest_b5a0e08_entities
     group by  YEAR(date), MONTH(date)
     order by  YEAR(date), MONTH(date);
+
+
+create view cumul as
+	select YEAR(date) as y, MONTH(date) as m, sum(incomes) as incomes, sum(outcomes) as outcomes, (sum(incomes) - sum(outcomes)) as total
+	    from invest_bb86840_entities
+	    group by  YEAR(date), MONTH(date)
+	    order by  YEAR(date), MONTH(date);
